7bf8f4ffc11f866162acad39cef95e9b02c9d866
Dpakkk <pokharelbikash981@gmail.com>
2025-03-20 15:50:08 -0400

    made logo smaller trusted by section

--- src/components/TrustedBy.tsx
+++ src/components/TrustedBy.tsx
@@ -5,13 +5,13 @@ import Image from 'next/image';
 const TrustedBy: React.FC = () => {
   // List of logo paths and names
     const logos = [
-    { src: '/trusted-by/aws.png', alt: 'aws', width: 100, height: 40 },
-    { src: '/trusted-by/olympianmotors.png', alt: 'oly', width: 110, height: 40 },
-    { src: '/trusted-by/wefunder.png', alt: 'wefunder', width: 120, height: 40 },
-    { src: '/trusted-by/clip-bike.png', alt: 'clip bike', width: 60, height: 40 },
-    { src: '/trusted-by/sarbham.png', alt: 'sarbham', width: 150, height: 80 },
-    // { src: '/logos/fico.svg', alt: 'FICO', width: 100, height: 40 },
-    // { src: '/logos/hugging-face.svg', alt: 'Hugging Face', width: 140, height: 40 },
+    { src: '/trusted-by/aws.png', alt: 'aws', width: 80, height: 32 },
+    { src: '/trusted-by/olympianmotors.png', alt: 'oly', width: 88, height: 32 },
+    { src: '/trusted-by/wefunder.png', alt: 'wefunder', width: 96, height: 32 },
+    { src: '/trusted-by/clip-bike.png', alt: 'clip bike', width: 48, height: 32 },
+    { src: '/trusted-by/sarbham.png', alt: 'sarbham', width: 120, height: 64 },
+    // { src: '/logos/fico.svg', alt: 'FICO', width: 80, height: 32 },
+    // { src: '/logos/hugging-face.svg', alt: 'Hugging Face', width: 112, height: 32 },
   ];
 
   return (
